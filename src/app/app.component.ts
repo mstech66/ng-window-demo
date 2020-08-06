ae6a149acee5d39553ec79de56c317bff3ffeec8
Manthan Mevada <manthanmevada66@gmail.com>
2020-08-06 20:28:57 +0530

    Added Project Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'windowdemo';
+  count = 0;
+  activeWindows = [];
+  currentActiveWindow = null;
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    const posLeft = this.currentActiveWindow.offsetLeft;
+    const posTop = this.currentActiveWindow.offsetTop;
+    if (event.key === 'ArrowUp') {
+      this.currentActiveWindow.style.marginTop = (posTop - 58) + 'px';
+      console.log(event.key);
+    }
+    if (event.key === 'ArrowDown') {
+      this.currentActiveWindow.style.marginTop = (posTop + 58) + 'px';
+      console.log(event.key);
+    }
+    if (event.key === 'ArrowLeft') {
+      this.currentActiveWindow.style.marginLeft = (posLeft - 58) + 'px';
+      console.log(event.key);
+    }
+    if (event.key === 'ArrowRight') {
+      this.currentActiveWindow.style.marginLeft = (posLeft + 58) + 'px';
+      console.log(event.key);
+    }
+    if (event.key === 'Delete') {
+      this.currentActiveWindow.parentNode.removeChild(this.currentActiveWindow);
+    }
+  }
+
+  addWindow() {
+    const window = {
+      id: `window${this.count}`,
+      index: this.count
+    };
+    this.activeWindows.push(window);
+    this.count = this.count + 1;
+  }
+
+  setWindowListener(id: string) {
+    this.currentActiveWindow = document.getElementById(id);
+  }
 }
